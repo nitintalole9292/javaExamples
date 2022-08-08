e0c81ceb96587dfc73d2ab93f1be7dec67da00f4
ntalole <ntalole@nitint-lap93>
2022-08-08 18:42:23 +0530

    This is my first commit project on open gite repository

---
+++ src/javaExamples/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaExamples;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Welcome To Nitin .....!!!");
+
+	}
+
+}
